e7f9f6e8654e1443d36c3a7de6560ea8eea6bc11
shawnjb <129807902+shawnjb@users.noreply.github.com>
2023-07-23 14:33:35 -0700

    Convert to module & utilize module for script.

--- ishere.lua
+++ ishere.lua
@@ -1,23 +1,2 @@
-if type(io) ~= 'table' or not io.write then
-    require('io')
-end
-
-local fn = arg[1]
-
-if type(fn) ~= 'string' or fn:match('^%s*$') then
-    io.write('File Availability Checker\nCopyright (c) shawnjb 2023\n')
-    return
-elseif fn == '-v' or fn == '--version' then
-    io.write('1.0.0')
-    return
-end
-
-local file, errorMessage = io.open(fn, "r")
-
-if file then
-    io.write(('The file \"%s\" is available'):format(fn))
-elseif type(errorMessage) == 'string' then
-    io.write(('Could not find the file \"%s\"'):format(fn) .. '\n' .. errorMessage .. '\n' .. debug.traceback())
-end
-
+require("isheremodule").validate(arg)
 os.exit()
---
+++ isheremodule.lua
@@ -0,0 +1,28 @@
+return {
+    validate = function(arg)
+        if type(io) ~= 'table' or not io.write then
+            require('io')
+        end
+
+        local fn = arg[1]
+
+        if type(fn) ~= 'string' or fn:match('^%s*$') then
+            io.write('File Availability Checker\nCopyright (c) shawnjb 2023\n')
+            return
+        elseif fn == '-v' or fn == '--version' then
+            io.write('1.0.0')
+            return
+        end
+
+        local file, errorMessage = io.open(fn, "r")
+
+        if file then
+            io.write(('The file \"%s\" is available'):format(fn))
+            return true
+        elseif type(errorMessage) == 'string' then
+            io.write(('Could not find the file \"%s\"'):format(fn) .. '\n' .. debug.traceback())
+        end
+        
+        return false
+    end
+}
